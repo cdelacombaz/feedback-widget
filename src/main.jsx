746aa14cda2ff627ee71570f20a9d8e2d83fe802
cdelacombaz <cdelacombaz@bluewin.ch>
2023-05-09 19:59:20 +0200

    added loop to check for feedback widget

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,35 @@ import App from './App.jsx'
 
 const feedbackWidgetRoot = document.getElementById('feedback-widget-root')
 console.log('feedbackWidgetRoot loading', feedbackWidgetRoot)
-if (feedbackWidgetRoot) {
-  const projectId = feedbackWidgetRoot.getAttribute('data-project-id')
-  const assigneeId = feedbackWidgetRoot.getAttribute('data-assignee-id')
-  const apiUrl = feedbackWidgetRoot.getAttribute('data-api-url')
-  const issueTypeId = feedbackWidgetRoot.getAttribute('data-issue-type-id')
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <App
-        projectId={projectId}
-        assigneeId={assigneeId}
-        apiUrl={apiUrl}
-        issueTypeId={issueTypeId}
-      />
-    </React.StrictMode>,
-    feedbackWidgetRoot
-  )
+const renderWidget = () => {
+  if (feedbackWidgetRoot) {
+    const projectId = feedbackWidgetRoot.getAttribute('data-project-id')
+    const assigneeId = feedbackWidgetRoot.getAttribute('data-assignee-id')
+    const apiUrl = feedbackWidgetRoot.getAttribute('data-api-url')
+    const issueTypeId = feedbackWidgetRoot.getAttribute('data-issue-type-id')
+    console.log(
+      'feedbackWidgetRoot found. Rendering widget.',
+      projectId,
+      assigneeId,
+      apiUrl,
+      issueTypeId
+    )
+    ReactDOM.render(
+      <React.StrictMode>
+        <App
+          projectId={projectId}
+          assigneeId={assigneeId}
+          apiUrl={apiUrl}
+          issueTypeId={issueTypeId}
+        />
+      </React.StrictMode>,
+      feedbackWidgetRoot
+    )
+  } else {
+    console.log('feedbackWidgetRoot not found. Retrying in 5 seconds.')
+    setTimeout(renderWidget, 5000)
+  }
 }
+
+renderWidget()
